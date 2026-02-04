a2e69f5bc87a96c7eea3dac5b27ac5bcfbba0630
Link4Electronics <link4electronics@gmail.com>
2026-02-04 09:23:17 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,8 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm doxygen \
+    pipewire-audio \
+    pipewire-jack  \
     rtmidi
 
 echo "Installing debloated packages..."
