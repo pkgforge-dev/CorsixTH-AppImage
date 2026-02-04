e4d7179632841bbb51dec3693ca1953964e294c3
Link4Electronics <link4electronics@gmail.com>
2026-02-04 09:40:01 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    alsa-plugins   \
     doxygen        \
     fluidsynth     \
     pipewire-audio \
