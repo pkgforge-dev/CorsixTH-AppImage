f09d5f6db51d93a27667873f84307cbf30cbac54
Link4Electronics <link4electronics@gmail.com>
2026-02-05 09:50:32 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm   \
+    cmake                 \
     doxygen               \
     ffmpeg                \
     fluidsynth            \
@@ -29,7 +30,7 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package
 
 # If the application needs to be manually built that has to be done down here
-echo "Making nightly build of Snes9x-GTK..."
+echo "Making nightly build of CorsixTH..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/CorsixTH/CorsixTH"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
